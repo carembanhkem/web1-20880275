835ae358d0fcb2bddc9cb86cb75d58a49215cd93
carembanhkem <20880275@student.hcmus.edu.vn>
2023-02-06 19:55:41 +0700

    Complete About Page

---
+++ .history/js/20880275_20230206095607.js
@@ -0,0 +1,14 @@
+const API = "https://web1-api.vercel.app/api";
+
+async function loadData(request, templateId, viewId){
+    const response = await fetch(`${API}/${request}`);
+    const data = await response.json();
+    //Compile a template in JavaScript by using Handlebars.compile
+    var source = document.getElementById().innerHTML;
+    var template = Handlebars.compile(source);
+    //Get the HTML result of evaluating a Handlebars template by executing the template with a context.
+    var context = { data: data };
+    //Display on view
+    var view = document.getElementById("products")
+    view.innerHTML = template(context);
+}
